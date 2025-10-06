3f3ebccab0e740bf3f688d5d34815a176a787dee
akubiakpl <260802@edu.p.lodz.pl>
2025-10-06 13:24:05 +0200

    zmiana mnożenia w metodzie Policz

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 
+int Policz(int a, int b) {
+    return a * b;
+}
 
 int main() {
 
     auto lang = "C++";
     std::cout << "Hello and welcome to " << lang << "!\n";
+    std:: cout <<"W drugim branchu; 2+3=" << Policz(2,3) <<std::endl;
 
 
     return 0;
